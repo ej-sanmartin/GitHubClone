8c0cdffcce9da22a7a4c1c04ef0ee9cfb001de24
Edgar J San Martin <edgar.sanmartinjr@gmail.com>
2021-05-25 16:59:31 -0400

    Working on header navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import './App.css';
+import { IconContext } from 'react-icons';
+import { FaGithub } from 'react-icons/fa';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+      <nav className="header">
+        <div className="nav-content">
+          <IconContext.Provider value={{ color: 'white' }}>
+            <FaGithub size={42} />
+          </IconContext.Provider>
+          <form className="search-form">
+            <input
+                className="search-form-input"
+                type="text"
+                id="search"
+                name="search"
+                placeholder="Search or jump to..."
+            />
+          </form>
+          <a href="https://github.com/pulls" target="_blank">Pull requests</a>
+          <a href="https://github.com/issues" target="_blank">Issues</a>
+          <a href="https://github.com" target="_blank">Codespaces</a>
+          <a href="https://github.com/marketplace" target="_blank">Marketplace</a>
+          <a href="https://github.com/explore" target="_blank">Explore</a>
+        </div>
+        <div className="nav-profile">
+
+        </div>
+      </nav>
+      
     </div>
   );
 }
