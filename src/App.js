29eaa86cd27f5912d94793ed4d4c87baa39d0157
Edgar J San Martin <edgar.sanmartinjr@gmail.com>
2021-05-26 06:29:28 -0400

    Started footer section of page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 import { IconContext } from 'react-icons';
-import { BiBookOpen, BiBarChartSquare } from 'react-icons/bi';
+import { BiBookOpen, BiBarChartSquare, BiGitRepoForked } from 'react-icons/bi';
 import { CgProfile } from 'react-icons/cg';
 import { FaGithub } from 'react-icons/fa';
-import { FiBook } from 'react-icons/fi';
+import { FiBook, FiStar } from 'react-icons/fi';
 import { GoPlusSmall } from 'react-icons/go';
 import { GrEmoji } from 'react-icons/gr';
 import { IoMdNotificationsOutline } from 'react-icons/io';
@@ -120,17 +120,28 @@ function App() {
         <article className="repo">
           <div className="repo-header">
             <a href="#"><h2>Repo-address</h2></a>
-            <button>Cool</button>
+            <a className="star-button">
+              <IconContext.Provider value={{ color: 'gray' }}>
+                <FiStar size={24} />
+              </IconContext.Provider>
+              <p>Star</p>
+            </a>
           </div>
           <p className="repo-description">Lorem-ipsum</p>
           <div className="repo-info">
-            <div className="language-item">
+            <div className="language-info">
               <p>Language</p>
             </div>
             <div className="star-info">
+              <IconContext.Provider value={{ color: 'gray' }}>
+                <FiStar size={24} />
+              </IconContext.Provider>
               <p>20</p>
             </div>
             <div className="fork-info">
+              <IconContext.Provider value={{ color: 'gray' }}>
+                <BiGitRepoForked size={24} />
+              </IconContext.Provider>
               <p>4</p>
             </div>
             <div className="updated-info">
@@ -140,7 +151,25 @@ function App() {
         </article>
       </section>
       <footer>
-
+        <a>© 2021 Github, Inc.</a>
+        <a href="https://docs.github.com/en/github/site-policy/github-terms-of-service" target="_blank" rel="noopener noreferrer">Terms</a>
+        <a href="https://docs.github.com/en/github/site-policy/github-privacy-statement" target="_blank" rel="noopener noreferrer">Privacy</a>
+        <a href="https://github.com/security" target="_blank" rel="noopener noreferrer">Security</a>
+        <a href="https://www.githubstatus.com/" target="_blank" rel="noopener noreferrer">Status</a>
+        <a href="https://docs.github.com/" target="_blank" rel="noopener noreferrer">Docs</a>
+        <div></div>
+        <a href="https://github.com/">
+          <IconContext.Provider href="https://github.com/" value={{ color: 'gray' }}>
+            <FaGithub className="nav-item-hover-effect" size={28} />
+          </IconContext.Provider>
+        </a>
+        <div></div>
+        <a href="https://support.github.com/" target="_blank" rel="noopener noreferrer">Contact Github</a>
+        <a href="https://github.com/pricing" target="_blank" rel="noopener noreferrer">Pricing</a>
+        <a href="https://docs.github.com/" target="_blank" rel="noopener noreferrer">API</a>
+        <a href="https://services.github.com/" target="_blank" rel="noopener noreferrer">Training</a>
+        <a href="https://github.blog/" target="_blank" rel="noopener noreferrer">Blog</a>
+        <a href="https://github.com/about" target="_blank" rel="noopener noreferrer">About</a>
       </footer>
     </div>
   );
